5f026c75aea8dfc3c09019767125835753503436
Mohamed Aziz Hlel <med.aziz.hlel@gmail.com>
2023-08-02 11:12:10 +0100

    Update DatabaseConnection.java

--- src/databaseConnection/DatabaseConnection.java
+++ src/databaseConnection/DatabaseConnection.java
@@ -3,13 +3,12 @@ package databaseConnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 public class DatabaseConnection {
-    public Connection databaseLink;
-
-    public Connection getConnection(){
-        String databaseName = "libraryproject";
-        String databaseUser = "root";
-        String databasePassword = "" ;
-        String url = "jdbc:mysql://localhost/" + databaseName;
+    public static Connection getConnection(){
+        private String databaseName = "libraryproject";
+        private String databaseUser = "root";
+        private String databasePassword = "" ;
+        private String url = "jdbc:mysql://localhost/" + databaseName;
+        private Connection databaseLink;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
